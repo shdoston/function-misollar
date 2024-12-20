c2da7aa182dc6bf2f7bf1ff695f1fb483ea57569
Dostonbek <dostonbek200gmail.com>
2024-12-20 15:54:01 +0500

    function misol

---
+++ app.js
@@ -0,0 +1,128 @@
+// 1
+// function sum (x,y) {
+// return x+y
+// }
+
+// let a = sum( 2,6)
+// console.log(a)
+// 2
+// function toSekunds(minutes,soniya) {
+//     return minutes*soniya
+// }
+// let result = toSekunds(456,12345)
+// console.log(result)
+// 3
+// function nextInteger(int,son) {
+//     return  int + son
+// }
+// let result = nextInteger(23,1)
+// console.log(result)
+// 4
+// function uchburchakYuzi(asos, balandlik) {
+//     return (asos*balandlik) /2
+// }
+// let result = uchburchakYuzi(3,2)
+// console.log(result)
+// 5
+// function ageToDays(age,kun) {
+//     return age*kun
+// }
+// let result = ageToDays(24,365)
+// console.log(result)
+// 6
+// function kub (kub,son) {
+// return kub**3
+// }
+// let result = kub(5,3)
+// console.log(result)
+// 7
+
+// function firstElement ([1, 2, 3]) {
+//     return 
+// }
+
+// 8
+// function power(x,y) {
+//     return x*y
+// }
+// let result = power(5,3)
+// console.log(result)
+// 9
+// function hourToSekunds(hour,sekund) {
+//     return hour* sekund
+// }
+// let result = hourToSekunds(5,60)
+// console.log(result)
+// 10
+// function uchinchiTomon(tomon1, tomon2) {
+//     return (tomon1+tomon2)-1
+// }
+// let result = uchinchiTomon(8,10)
+// console.log(result)
+// 11
+// function qoldiq(son1, son2) {
+//     return son1%son2
+// }
+// let result = qoldiq(1,3)
+// console.log(result)
+// 12
+// function turtburchakYuzi(boyi, eni) {
+//     return boyi * eni
+// }
+// let result = turtburchakYuzi(,3)
+// console.log(result)
+// 13
+// function stringQoshish(a) {
+//     return `something ${a}`
+// }
+// let result = stringQoshish('Bob Jane')
+// console.log(result)
+// 14
+// function noldan(raqam) {
+//  if (raqam>=0 ){
+//     console.log(true)
+//  } else {
+// console.log(false)
+//  }
+
+// 16
+// function ichkiBurchaklar(n) {
+//     return (n - 2) * 180
+// }
+// let result = ichkiBurchaklar(3)
+// console.log(result)
+// 17
+// function ochkolar(x,y) {
+//     return x+y 
+// }
+// let result = ochkolar(2,5)
+// console.log(result)
+// 18
+// function sum (son) {
+// return `Mubashir${son}`
+// }
+// let result = sum('Edabit')
+// console.log(result)
+// 19
+// function ikkitaSon(x,y) {
+//     son = `${x+y}`
+//     if(son<100){
+//         console.log(true)
+//     } else{
+//         console.log(false)
+//     }
+// }
+// ikkitaSon(56,5)
+// 20
+
+function printArray () {
+    for (let i = 1; i<=6; i++){
+        console.log(i)
+    }
+}
+printArray()
+
+
+
+
+
